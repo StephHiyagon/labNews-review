f8b765d20ae5e8178612cf84a63ac7f4141b9456
Katherine Ortega <katy_ja2915@hotmail.com>
2017-07-10 20:02:25 -0500

    arreglando merge

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -312,12 +312,21 @@ const MainNews = (update) => {
 const MundoNews = (update) => {
     const container = $('<div class="container-mundo"></div>');
     const row = $('<div class="row"></div>');
+    const group = $('<div class="col l6 s12"></div>');
 
-    state.news.forEach((i) => {
+    state.news.forEach((i, index) => {
         if (state.categories[1].id == i.categories[0]) {
-            row.append(switcher(i));
+            if (index == 5 || index == 6 || index == 8 || index == 9) {
+                group.append(switcher(i));
+            } else if (index == 4) {
+                row.prepend(switcher(i));
+            }else {
+                row.append(switcher(i));
+            }
         }
     });
+
+    row.prepend(group);
 
 
     container.append(row);
